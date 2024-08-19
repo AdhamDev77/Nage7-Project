d80b1c9ba4784bdc81ecaf583d49fff79d40b535
Adham <112887020+AdhamDev77@users.noreply.github.com>
2024-08-19 03:28:01 +0300

    my changes

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, ClerkMiddlewareOptions } from "@clerk/nextjs/server";
 export default clerkMiddleware({
   // You can still use options for Clerk here if needed
 } as ClerkMiddlewareOptions);
-
+//a
 export const config = {
   matcher: [
     "/((?!.*\\..*|_next).*)",
